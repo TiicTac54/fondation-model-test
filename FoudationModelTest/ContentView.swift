a21d5450ca3d4f86637d9d8dc1c5a57d65b5a886
TiicTac <pierik12landry@gmail.com>
2026-02-28 11:44:54 -0500

    Initial Commit

---
+++ FoudationModelTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoudationModelTest
+//
+//  Created by Piérik Landry on 2026-02-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoudationModelTest/FoudationModelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoudationModelTestApp.swift
+//  FoudationModelTest
+//
+//  Created by Piérik Landry on 2026-02-28.
+//
+
+import SwiftUI
+
+@main
+struct FoudationModelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
